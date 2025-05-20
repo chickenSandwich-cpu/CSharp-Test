b270b36ab9067c57e66f90f89d3aa191e13992e7
SaltedFries <tranblueneverdie@gmail.com>
2025-05-20 13:50:10 -0700

    interesting syxtan

--- C# Test/Program.cs
+++ C# Test/Program.cs
@@ -1,13 +1,8 @@
-﻿int x = 2;
+﻿
+double x = 2.5;
+int y = (int)x; // Explicit cast from double to int
+int z = Convert.ToInt32(x); // Convert double to int using Convert class
+
 Console.WriteLine(x);
-
-double y = 2.5;
 Console.WriteLine(y);
-
-float z = 4.5f;
 Console.WriteLine(z);
-
-bool isTrue = true;
-Console.WriteLine(isTrue);
-
-Console.ReadKey();
