f1e1717fc781e00d1cf3efc40b5a639f7153b7d6
SaltedFries <tranblueneverdie@gmail.com>
2025-05-20 13:39:22 -0700

    Add project files.

---
+++ C# Test/Program.cs
@@ -0,0 +1,13 @@
+﻿int x = 2;
+Console.WriteLine(x);
+
+double y = 2.5;
+Console.WriteLine(y);
+
+float z = 4.5f;
+Console.WriteLine(z);
+
+bool isTrue = true;
+Console.WriteLine(isTrue);
+
+Console.ReadKey();
