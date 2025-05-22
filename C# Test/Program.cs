4de48c64a2296be3b5d43582a0b0f6709721c085
SaltedFries <tranblueneverdie@gmail.com>
2025-05-22 14:06:15 -0700

    cheat code for june

--- C# Test/Program.cs
+++ C# Test/Program.cs
@@ -1,5 +1,20 @@
-﻿for (int i = 25; i < 32; i++)
+﻿for (int i = 1; i < 20; i++)
 {
-    Console.Write("- " + "May ");
-    Console.WriteLine(i + "th" + ": ...");
+    Console.Write("- " + "June ");
+    if (i == 1 || i == 31)
+    {
+        Console.WriteLine(i + "st" + ": ...");
+    }
+    else if (i == 2 || i == 22)
+    {
+        Console.WriteLine(i + "nd" + ": ...");
+    }
+    else if (i == 3 || i == 23)
+    {
+        Console.WriteLine(i + "rd" + ": ...");
+    }
+    else
+    {
+        Console.WriteLine(i + "th" + ": ...");
+    }
 }
