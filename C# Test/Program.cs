651362e65f013da7d4c09009646fc3cc32e26be3
SaltedFries <tranblueneverdie@gmail.com>
2025-05-22 14:03:25 -0700

    (cheat) code for may

--- C# Test/Program.cs
+++ C# Test/Program.cs
@@ -1,8 +1,5 @@
-﻿Console.Write("Enter the first side: ");
-double side1 = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("Enter the second side: ");
-double side2 = Convert.ToDouble(Console.ReadLine());
-
-double side3 = Math.Sqrt(Math.Pow(side1, 2) + Math.Pow(side2, 2));
-Console.WriteLine($"The length of the hypotenuse is: {side3}");
+﻿for (int i = 25; i < 32; i++)
+{
+    Console.Write("- " + "May ");
+    Console.WriteLine(i + "th" + ": ...");
+}
